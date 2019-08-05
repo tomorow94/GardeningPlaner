17035ffc9ac0c15df2f3f3506a885c2a64fcf1d8
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-08-05 12:35:46 +0200

    ADDED two more eavaluation tests

--- app/src/test/java/de/GardeningPlaner/Model/Crops/MixedCultureEvaluatorTest.java
+++ app/src/test/java/de/GardeningPlaner/Model/Crops/MixedCultureEvaluatorTest.java
@@ -7,10 +7,26 @@ import static org.junit.Assert.*;
 public class MixedCultureEvaluatorTest {
 
     @Test
+    public void evaluateTolerance_Aubergine_Aubergine_ShouldBeNUTRAL() {
+        CropTolerance expected = CropTolerance.NUTRAL;
+        MixedCultureEvaluator evaluator = new MixedCultureEvaluator();
+        CropTolerance actual = evaluator.evaluateTolerance(new Aubergine(), new Aubergine());
+        assertEquals(expected,actual);
+    }
+
+    @Test
     public void evaluateTolerance_Aubergine_BushBean_ShouldBeNUTRAL() {
         CropTolerance expected = CropTolerance.NUTRAL;
         MixedCultureEvaluator evaluator = new MixedCultureEvaluator();
         CropTolerance actual = evaluator.evaluateTolerance(new Aubergine(), new BushBean());
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void evaluateTolerance_Aubergine_ChineseCabbage_ShouldBeNUTRAL() {
+        CropTolerance expected = CropTolerance.NUTRAL;
+        MixedCultureEvaluator evaluator = new MixedCultureEvaluator();
+        CropTolerance actual = evaluator.evaluateTolerance(new Aubergine(), new ChineseCabbage());
         assertEquals(expected,actual);
     }
 }
