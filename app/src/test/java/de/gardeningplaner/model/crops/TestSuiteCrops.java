d7a3ebaed827e87474552c9e1ec5066885edf45e
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-08-07 17:01:32 +0200

    Update TestSuiteCrops.java

--- app/src/test/java/de/gardeningplaner/model/crops/TestSuiteCrops.java
+++ app/src/test/java/de/gardeningplaner/model/crops/TestSuiteCrops.java
@@ -3,8 +3,6 @@ package de.gardeningplaner.model.crops;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import de.gardeningplaner.model.MixedCultureEvaluatorTest;
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
         {
